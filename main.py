293d5a7251d497058d54dfdd5a038f3c7c0d12e2
Aaron Long <aaron.long.c@gmail.com>
2025-11-16 15:23:48 -0500

    feat: Iterate on playlist loading

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ import re
 from xml.sax.saxutils import escape
 
 import requests
-import spotipy
 
 import core_utils
-from spotify_utils import authenticate_spotify
+from spotify_utils import authenticate_spotify, steam_playlist
 
 logger = core_utils.setup_logger()
 parser = argparse.ArgumentParser()
@@ -20,12 +19,9 @@ def main():
     args = parser.parse_args()
     _output_path = core_utils.handle_output_file(args.output)
     creds = core_utils.prompt_credentials()
-    auth = authenticate_spotify(creds)
-    sp = spotipy.Spotify(auth_manager=auth)
-    results = sp.current_user_saved_tracks()
-    for idx, item in enumerate(results["items"]):
-        track = item["track"]
-        print(idx, track["artists"][0]["name"], " – ", track["name"])
+    sp = authenticate_spotify(creds)
+    playlist = [playlist for playlist in steam_playlist(sp)]
+    logger.info(f"There were {len(playlist)} for user")
 
 
 if __name__ == "__main__":
--- spotify_utils.py
+++ spotify_utils.py
@@ -1,3 +1,4 @@
+from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
 from core_utils import SpotifyClientAuth
@@ -5,10 +6,30 @@ from core_utils import SpotifyClientAuth
 
 def authenticate_spotify(
     auth: SpotifyClientAuth, scope="playlist-read-private,user-library-read"
-):
-    return SpotifyOAuth(
+) -> Spotify:
+    oauth = SpotifyOAuth(
         client_id=auth.client_id,
         client_secret=auth.client_secret,
         redirect_uri=auth.redirect_url,
-        scope="playlist-read-private,user-library-read",
+        scope=scope,
     )
+    return Spotify(auth_manager=oauth)
+
+
+def steam_playlist(sp: Spotify, *, limit=50, offset=0):
+    playlist = sp.current_user_playlists(limit, offset)
+    total = playlist["total"]
+    items = playlist["items"]
+    cur = len(items)
+
+    yield from items
+
+    if cur >= total:
+        return
+
+    while cur < total:
+        offset += 1
+        playlist = sp.current_user_playlists(limit, offset)
+        items = playlist["items"]
+        cur += len(items)
+        yield from items
